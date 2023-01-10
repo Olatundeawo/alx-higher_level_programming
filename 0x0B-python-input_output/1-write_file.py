8deabe24c543342c5be93a067830957d224c62a9
Olatundeawo <awotimilehin.babatunde>
2023-01-10 08:38:24 -0800

    modify

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,6 +8,6 @@ function that writes into a txt file
 
 def write_file(filename="", text=""):
     """ print a text file"""
-    with open(filename, mode = 'w', encoding='UTF-8')as file:
 
+    with open(filename, mode = 'w', encoding='UTF-8')as file:
         return file.write(text)
