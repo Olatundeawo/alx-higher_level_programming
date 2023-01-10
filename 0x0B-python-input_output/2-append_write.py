712c941e1ef5497a6b3d3cf1c16f863bf9f9e36c
Olatundeawo <awotimilehin.babatunde>
2023-01-10 08:35:11 -0800

    modify

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
 """ Module that contains a function that appends to a text file
 """
+
 
 def append_write(filename="", text=""):
     """ Function that appends to a text file
@@ -11,5 +11,6 @@ def append_write(filename="", text=""):
     Raises
         Exception: when the file can be opened
     """
-    with open(filename,'a', encoding="utf-8") as file:
-        return file.write(text)
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
