ffe14b1921cdc2f15804e35e8a349fb2187d9281
Olatundeawo <awotimilehin.babatunde>
2023-01-10 08:30:20 -0800

    modify

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -11,6 +11,5 @@ def append_write(filename="", text=""):
     Raises
         Exception: when the file can be opened
     """    
-    with open(filename,'a', encoding='UTF-8') as file:
-
+    with open(filename,'a', encoding='utf-8') as file:
         return file.write(text)
