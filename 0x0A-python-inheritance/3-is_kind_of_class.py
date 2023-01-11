cab7c9a9eac61822627d3a02f27ff6c86a383a71
Olatundeawo <awotimilehin.babatunde>
2023-01-11 02:11:42 -0800

    modify

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+check maybe isinstance of the obj
+"""
+
+def is_kind_of_class(obj,a_class):
+    """ function that return instance of an obj """
+    return (isinstance(obj,a_class))
