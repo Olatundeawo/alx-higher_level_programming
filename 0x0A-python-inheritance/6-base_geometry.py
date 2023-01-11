4d20e72ede501f23993b04e4f389798f39619885
Olatundeawo <awotimilehin.babatunde>
2023-01-11 02:41:56 -0800

    modify

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """
-class that raise an exception
+6-base_geometry.py
+Write a class BaseGeometry (based on 5-base_geometry.py).
 """
 
 
 class BaseGeometry:
+    """Public instance method: def area(self):
+    that raises an Exception with the message area()
+    is not implemented"""
     def area(self):
-        """ return exception """
+        """return exeption"""
         raise Exception("area() is not implemented")
