031f6dc6b13ed8d4f30aeb664d45b2edd52dfd85
Olatundeawo <awotimilehin.babatunde>
2023-01-11 02:05:48 -0800

    added a2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,7 +6,7 @@ Function that check for instance
 
 def is_same_class(obj, a_class):
     """check for maybe instance is equal """
-    if isinstance(obj, a_class):
+    if type(obj) == a_class:
         return True
     else:
         return False
