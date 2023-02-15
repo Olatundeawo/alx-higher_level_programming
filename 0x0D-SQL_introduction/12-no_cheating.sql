4a85c2e335b93935f06971199b29ac5d2c041da0
Olatundeawo <awotimilehin.babatunde>
2023-02-14 16:13:35 -0800

    added some files

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- a script that update a list
+UPDATE second_table SET score =10 WHERE name = 'Bob';
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- a script that the compute the averagre score
+SELECT AVG(score) AS average FROM second_table;
