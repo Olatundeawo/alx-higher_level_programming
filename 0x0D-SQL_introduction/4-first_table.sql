eca1e81990534e0b29f3f2c430df8e7bf1c3523f
Olatundeawo <awotimilehin.babatunde>
2023-02-16 19:18:50 -0800

    modify

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- a script that create a table with soe values
-CREATE TABLE IF NOT EXIT first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
