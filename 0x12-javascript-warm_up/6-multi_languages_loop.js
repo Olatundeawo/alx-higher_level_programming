ca6b997733743063a16259e6e4d01c3e9665982c
Olatundeawo <awotimilehin.babatunde>
2023-03-13 16:24:05 -0700

    mod

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 
 const line = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 let i;
-for (i = 0; i < line.length; i++) {
+for (i = 0; i < line.leniigth; i++) {
   console.log(line[i]);
 }
