2d20d16c9e97074f8a17608a96a9b0220076df50
Olatundeawo <awotimilehin.babatunde>
2023-03-13 07:16:39 -0700

    modify

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let lines = ["C is fun","Python is cool", "JavaScript is amazing"];
+let lines = ['C is fun','Python is cool', 'JavaScript is amazing'];
 
 console.log(lines[0],lines[1], lines[2]);
 
