ce3fef078f5e84c7778ba661b5f512b3019d3f6a
Olatundeawo <awotimilehin.babatunde>
2023-03-13 07:23:42 -0700

    modify

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let lines = ['C is fun','Python is cool', 'JavaScript is amazing'];
+let lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-console.log(lines[0],lines[1], lines[2]);
+console.log(lines[0]\n,lines[1]\n, lines[2]);
 
