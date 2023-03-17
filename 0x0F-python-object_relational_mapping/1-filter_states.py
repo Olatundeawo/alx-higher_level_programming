b5570ceb93c5fab1003e2e1ea1e6731a4c4d0e86
Olatundeawo <awotimilehin.babatunde>
2023-03-17 09:45:43 -0700

    added somefiles

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+accepts 3 arguments
+and lists all states with a name N as the beginning
+"""
+
+import sys
+import MySQLdb
+
+
+def main(argv):
+    """list states with name starting with N"""
+    db = MySQLdb.connect(host="locahost", port=3306, user=argv[1], password=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute = ("SELECT * FROM states WHERE name LIKE BINARY 'N%'\ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        main(sys.argv)
+
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+script that takes four arguments and
+all values where name matches the 4th argument
+"""
+import sys
+import MySQLdb
+
+def main(argv):
+    """display values that matches the 4th argument"""
+    db = MySQLdb.connect(host="localhost",port=3306, user=argv[1], password=argv[2], db=argv[3],searched=argv[4])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}%'\ ORDER BY id ASC".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 5:
+        main(sys.argv)
