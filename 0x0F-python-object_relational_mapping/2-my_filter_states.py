966bdd866d75da46939d4d3d0b2b130c0a70d707
Olatundeawo <awotimilehin.babatunde>
2023-03-17 10:35:28 -0700

    modify

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,9 +8,7 @@ import MySQLdb
 
 
 def main(argv):
-    """display the name that matches the 4th 
-        argument
-    """
+    """display the name that matches the 4th argument"""
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
@@ -19,6 +17,7 @@ def main(argv):
     rows = cur.fetchall()
     for row in rows:
         print(row)
+    pass
     cur.close()
     db.close()
 
