e96fc693aa9fb307243198cdc33e4620eb53ace0
Olatundeawo <awotimilehin.babatunde>
2023-03-17 00:33:18 -0700

    modify

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ import sys
 
 def main(argv):
     """ link all the 3 arguments to something """
-    db = MySQLdb.connect(host="localhost", port=3306, 
+    db = MySQLdb.connect(host="localhost", port=3306,
                          user= argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
--- 0x0F-python-object_relational_mapping/0-select_states.sql
+++ 0x0F-python-object_relational_mapping/0-select_states.sql
@@ -7,3 +7,4 @@ CREATE TABLE IF NOT EXISTS states (
 );
 
 INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+
