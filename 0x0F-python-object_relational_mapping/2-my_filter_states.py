8994c1f44d1e5d97d098b879fe9c019ec12c6d0b
Olatundeawo <awotimilehin.babatunde>
2023-03-17 09:53:32 -0700

    modify

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,9 +9,11 @@ import MySQLdb
 
 def main(argv):
     """display values that matches the 4th argument"""
-    db = MySQLdb.connect(host="localhost",port=3306, user=argv[1], password=argv[2], db=argv[3],searched=argv[4])
+    db = MySQLdb.connect(host="localhost",port=3306, user=argv[1], 
+                          password=argv[2], db=argv[3],searched=argv[4])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}%'\ ORDER BY id ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}%'\ 
+                ORDER BY id ASC".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
