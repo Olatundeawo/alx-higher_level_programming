d747b98b7eddb4cee7b15fda7f152ae1acd8ba58
Olatundeawo <awotimilehin.babatunde>
2023-03-17 00:22:47 -0700

    modify git push

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,6 @@ import sys
 
 def main(argv):
     """ link all the 3 arguments to something """
-
     db = MySQLdb.connect(host="localhost", port=3306, 
             user= argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
