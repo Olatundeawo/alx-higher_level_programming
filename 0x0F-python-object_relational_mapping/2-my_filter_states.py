187674bf5773981de3554088bed0ca62dcc4c4c2
Olatundeawo <awotimilehin.babatunde>
2023-03-17 10:23:41 -0700

    modify

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-
 """
-script that takes four arguments and
-all values where name matches the 4th argument
+take 4 arguments with no validation
+and display name that matches the 4th argument
 """
 import sys
 import MySQLdb
 
+
 def main(argv):
-    """display values that matches the 4th argument"""
-    db = MySQLdb.connect(host="localhost",port=3306, user=argv[1], 
-                          password=argv[2], db=argv[3],searched=argv[4])
+    """display the name that matches the 4th argument"""
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}%'\  ORDER BY id ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
