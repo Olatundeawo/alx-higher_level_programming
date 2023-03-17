ee88d6bbf6713f63e098834cc33314092419b83b
Olatundeawo <awotimilehin.babatunde>
2023-03-16 23:18:46 -0700

    added some iles

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+take 3 arguments with no validation
+"""
+import mySQLdb
+import sys
+
+def main(argv):
+    """ link all the 3 arguments to something """
+
+    db = mySQLdb.connect(host="localhost", port=3306,user= argv[1], password=argv[2], db=argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        main(sys.argv)
