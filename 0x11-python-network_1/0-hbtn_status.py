ec803a264421fb9f2daa02b26ee5cb6d12cafa7a
Olatundeawo <awotimilehin.babatunde>
2023-04-08 21:11:40 -0700

    mod

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urlopen(request) as response:
         body = response.read()
         print("Body response:")
-        print("-type:", type(body))
-        print("-content:", body)
-        print("-utf8 content:",body.decode("utf-8"))
+        print("- type:", type(body))
+        print("- content:", body)
+        print("- utf8 content:",body.decode("utf-8"))
