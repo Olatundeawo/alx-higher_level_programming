5dfb9e9a1d4b520ccbfb5ef8e7eab53f7e3e266a
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-04-25 10:42:02 -0700

    mod

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@
 
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (err, response) => {
-	if (err) {
-	console.error(err);
-	} else {
-	console.log(response);
-	}
+  if (err) {
+   console.error(err);
+   } else {
+   console.log(response);
+   }
 });
