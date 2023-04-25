32459441188d7a160f11816a7083c63f8a21a033
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-04-25 10:55:52 -0700

    modifty

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,6 +2,6 @@
 // a script that writes a string to a file.
 
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err,response) => {
-  if (err) console.log(err);
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err, response) => {
+  if (err) console.error(err);
 });
