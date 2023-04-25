a2b3822803298cec7e6897db980e67f3dcf64731
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-04-25 10:21:58 -0700

    modifiied

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 // a script that writes a string to a file.
 
-
 const fs = require('fs');
-
-
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8',(err,response)=> {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8',(err,response) => {
 	if (err) {
 		console.error(err);
-		return;
 	}
 	console.log('Success');
 });
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,6 @@
 // a script that display the status code of a GET request.
 
 const request = require('request');
-
 request(process.argv[2], (err, response) => {
 	if (err) {
 		console.error(err);
