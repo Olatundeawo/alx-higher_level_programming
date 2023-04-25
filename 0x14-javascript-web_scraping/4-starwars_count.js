825c73280341a2356e02514800ca3eb65ffd7907
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-04-25 11:37:03 -0700

    added a file

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+request.get(process.argv[2], (err, response, body) => {
+  if (err) console.error(err);
+  else if (response.statusCode == 200) {
+    let count = 0;
+    const results = JSON.parse(body).results;
+    const people = '/api/people/18/';
+    for (let i = 0; i < results.length; i++) {
+      if (results[i].characters.find(add => add.includes(people))) {
+	i++;
+      }
+    }
+    console.log(count);
+  }
+});
