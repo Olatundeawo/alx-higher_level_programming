20336cac5488458a76d0ea8254a14357d1bc920f
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-04-27 00:15:53 -0700

    modifty

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-requestUrl = 'https://swapi-api.alx-tools.com/api/films/';
+const requestUrl = 'https://swapi-api.alx-tools.com/api/films/';
 request(requestUrl + process.argv[2], (err, response, body) => {
   if (err) console.error(err);
   else console.log(JSON.parse(body).title);
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,8 +2,8 @@
 const request = require('request');
 request.get(process.argv[2], (err, response, body) => {
   if (err) console.error(err);
-  else if (response.statusCode == 200) {
-    let count = 0;
+  else if (response.statusCode === 200) {
+    const count = 0;
     const results = JSON.parse(body).results;
     const people = '/api/people/18/';
     for (let i = 0; i < results.length; i++) {
